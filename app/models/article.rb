3d315d53b1b6b99f450379e9e0d5ebb4f9b51b2b
anshika0112 <anshikabansal694@gmail.com>
2022-01-28 20:32:15 +0530

    Created User Model and formed association with Article Model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    belongs_to :user
     validates :title, presence: true,length:{minimun:3,maximum:35}
     validates :description,presence: true,length:{minimum:6,maximum:300}
 end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_many :articles
+    has_secure_password
+    validates :username, presence:true, length:{minimum:3, maximum:25}, uniqueness: true
+    VALID_EMAIL_REGEX= /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    validates :email, presence:true, length:{maximum:105}, uniqueness: true, format:{with: VALID_EMAIL_REGEX}
+end
